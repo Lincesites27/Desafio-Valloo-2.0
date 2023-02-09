ee64dac83e5025f9330845309341fbae4a7e7b41
Lincesites27 <contato@lincesites.com.br>
2023-02-09 16:47:20 -0300

    env e readme

--- backend/src/controller/connection.js
+++ backend/src/controller/connection.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
 
+require('dotenv').config();
+
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "cartoes"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
   });
 
   module.exports = connection;
